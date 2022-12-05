e7b7a5fb909a1f14de7734faf96c50963e64826f
Konrad <konrad.klein1@iu-study.org>
2022-12-05 22:41:28 +0100

    create a first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# import argparse
+#
+# def say_hello(args):
+#     if args.name:
+#         print(f'Hello {args.name}!')
+#
+#     else:
+#         print(f'Hallo Stranger')
+#
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--name')
+#     args = parser.parse_args()
+#     say_hello(args)
+#
+# if __name__ == '__main__':
+#     main()
+
