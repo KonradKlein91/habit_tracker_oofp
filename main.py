05b59024582c8bcdd3972f99eb9f5d981d2d30fb
Konrad <konrad.klein1@iu-study.org>
2022-12-05 23:55:50 +0100

    create templates for files

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# import argparse
-#
-# def say_hello(args):
-#     if args.name:
-#         print(f'Hello {args.name}!')
-#
-#     else:
-#         print(f'Hallo Stranger')
-#
-# def main():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--name')
-#     args = parser.parse_args()
-#     say_hello(args)
-#
-# if __name__ == '__main__':
-#     main()
+import argparse
 
+class habit:
+    def __init__(self, name, periodicity):
+        self.habit_name = name
+        self.periodicity = periodicity
+
+def say_hello(args):
+    if args.name:
+        print(f'Hello {args.name}!')
+
+    else:
+        print(f'Hallo Stranger')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name')
+    args = parser.parse_args()
+    say_hello(args)
+
+
+if __name__ == '__main__':
+    main()
