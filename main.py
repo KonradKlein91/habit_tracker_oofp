0a386e74ecc64907c345632a863ae0771b091d22
Konrad <konrad.klein1@iu-study.org>
2022-12-10 23:42:36 +0100

    fire implementation

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ import database
 
 
 class Habit:
-    def __init__(self, habit_name):
+
+    def __init__(self):
+        self.habit_name = ()
+    def create_habit(self, habit_name):
         self.habit_name = habit_name
-
-    def create_habit(self):
         database.insert_habit(self.habit_name)
-        print(f"Habit {self.habit_name} has been created and has been added to the database")
-
-
-hab_5 = Habit("Cooking")
-hab_5.create_habit()
-database.get_habits()
+        return print(f"Habit {self.habit_name} has been created and has been added to the database")
 
 
 if __name__ == "__main__":
-    fire.Fire()  # Call main function
+    fire.Fire(Habit)  # Call main function
